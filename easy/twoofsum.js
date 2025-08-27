f07cde450494c5e0a741afcd68b7f9c30b74b06c
Alan <Alan2003@sju.ac.kr>
2025-08-27 17:02:38 +0900

    twofosum

--- easy/twoofsum.js
+++ easy/twoofsum.js
@@ -11,3 +11,22 @@ var twoSum = function (nums, target) {
     }
   }
 };
+
+// tested using compiler
+
+const nums = [2, 12, 2, 10, 12];
+target = 24;
+
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      let result = nums[i] + nums[j];
+      if (result === target) {
+        console.log(result, "index:", i, j);
+        return [i, j];
+      }
+    }
+  }
+};
+
+twoSum(nums, target);
