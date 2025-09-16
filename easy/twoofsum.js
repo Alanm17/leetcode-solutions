a8671bd5177ce69e6e7fee83bbf79d18f80cf930
Alan <Alan2003@sju.ac.kr>
2025-09-16 19:47:02 +0900

    twoSum solved using O(n) with two ddifferent solutions HashMap and Map

--- easy/twoofsum.js
+++ easy/twoofsum.js
@@ -1,7 +1,7 @@
 // twoSum Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 
-var twoSum = function (nums, target) {
+var twoSum1 = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       let result = nums[i] + nums[j];
@@ -10,6 +10,16 @@ var twoSum = function (nums, target) {
       }
     }
   }
+};
+
+var twoSum = function (nums, target) {
+  let count = 0;
+  let result;
+  let map = new Map();
+
+  while (count) result = nums[count] + nums[count + 1];
+  if (result === target) return [nums[count], nums[count + 1]];
+  count++;
 };
 
 // tested using compiler
@@ -30,3 +40,30 @@ const twoSum = function (nums, target) {
 };
 
 twoSum(nums, target);
+
+///another way using O(n)
+
+const twoSum = function (nums, target) {
+  let map = new Map();
+  let compliment;
+  for (let i = 0; i < nums.length; i++) {
+    compliment = target - nums[i];
+    if (map.has(compliment)) {
+      return [map.get(compliment), i];
+    }
+    map.set(nums[i], i);
+  }
+};
+
+// one more way of doing it
+const twoSum = function (nums, target) {
+  let map = {};
+  let compliment;
+  for (let i = 0; i < nums.length; i++) {
+    compliment = target - nums[i];
+    if (map[compliment] !== undefined) {
+      return [map[compliment], i];
+    }
+    map[nums[i]] = i;
+  }
+};
