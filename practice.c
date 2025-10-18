96382f2d36841670b8adc325517b35776312b10c
Alan <Alan2003@sju.ac.kr>
2025-10-18 23:43:58 +0900

    Selection Sort

--- practice.c
+++ practice.c
@@ -46,6 +46,19 @@ void BubbleSort(int arr[],int n){
     }
     }
 }
+void SelectionSort(int arr[], int n){
+    for(int i = 0; i < n -1; i++){
+        int min = i;
+        for(int j = i +1; j<n; j++){
+            if(arr[min]>arr[j]){
+                min = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
 int main() {
     // Write C code here
     int arr[] ={23,32,4,56,23,12,4};
